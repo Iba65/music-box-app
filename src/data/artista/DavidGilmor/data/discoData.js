28b022688816b2b35e174e522af5887961948465
Iba65 <front.developer.iba@gmail.com>
2022-09-29 12:41:02 +0200

    your msg

---
+++ src/data/artista/DavidGilmor/data/discoData.js
@@ -0,0 +1,72 @@
+const discosData = [
+    {
+        disco: "On An Island",
+        portada: "dg2006/Portada.jpg",
+        anyo: "2006",
+        formato: "cd",
+        soporte: "digital",
+        canciones: [
+            {
+                imag: "",
+                titulo: "Castellorizon.",
+                duracion: "3:54",
+                url: "OnAnIsland/David_Gilmour_-_01_-_Castellorizon.mp3",
+            },
+            {
+                imag: "",
+                titulo: "On an Island.",
+                duracion: "6:47",
+                url: "OnAnIsland/David_Gilmour_-_02_-_On_An_Island.mp3",
+            },
+            {
+                imag: "",
+                titulo: "The Blue.",
+                duracion: "5:26",
+                url: "OnAnIsland/David_Gilmour_-_03_-_The_Blue.mp3",
+            },
+            {
+                imag: "",
+                titulo: "Take a Breath.",
+                duracion: "5:46",
+                url: "OnAnIsland/David_Gilmour_-_04_-_Take_A_Breath.mp3",
+            },
+            {
+                imag: "",
+                titulo: "Red Sky at Night.",
+                duracion: "2:51",
+                url: "OnAnIsland/David_Gilmour_-_05_-_Red_Sky_At_Night.mp3",
+            },
+            {
+                imag: "",
+                titulo: "This Heaven.",
+                duracion: "4:24",
+                url: "OnAnIsland/David_Gilmour_-_06_-_This_Heaven.mp3",
+            },
+            {
+                imag: "",
+                titulo: "Then I Close My Eyes.",
+                duracion: "5:26",
+                url: "OnAnIsland/David_Gilmour_-_07_-_Then_I_Close_My_Eyes.mp3",
+            },
+            {
+                imag: "",
+                titulo: "Smile.",
+                duracion: "4:03",
+                url: "OnAnIsland/David_Gilmour_-_08_-_Smile.mp3",
+            },
+            {
+                imag: "",
+                titulo: "A Pocketful of Stones.",
+                duracion: "6:17",
+                url: "OnAnIsland/David_Gilmour_-_09_-_A_Pocketful_Of_Stones.mp3",
+            },
+            {
+                imag: "",
+                titulo: "Where We Start.",
+                duracion: "6:45",
+                url: "OnAnIsland/David_Gilmour_-_10_-_Where_We_Start.mp3",
+            },
+        ],
+        videos: [],
+    },
+]
